9800767115e06695ca803eb8490fb73c9f443f00
poperdimka <83829174+poperdimka@users.noreply.github.com>
2021-09-22 21:57:29 +0300

    херовая хотьба кота

--- Assets/Scripts/cmove.cs
+++ Assets/Scripts/cmove.cs
@@ -6,18 +6,46 @@ public class cmove : MonoBehaviour
 {
     public float speed;
     private float moveInput;
+    private bool facingRight = true;
+    public Animator anim;
     private Rigidbody2D rb;
 
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
     private void FixedUpdate()
     {
 
         moveInput = Input.GetAxis("Horizontal1");
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+        if(moveInput != 0)
+        {
+            anim.Play("CatWalk");
+        }
+        else
+        {
+            anim.Play("CatStay");
+        }
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
 
     }
 }
